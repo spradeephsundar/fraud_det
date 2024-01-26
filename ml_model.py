2342168e9c9e704e4b443362d316162a35e55c08
spradeephsundar <s.pradeephsundar@gmail.com>
2024-01-26 10:17:01 +0530

    added 3 new ML model codes

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,15 @@
 
 
 print("adding all ML related cpde here")
+
+
+def model1():
+    print("adding code for ML Model1")
+
+
+def model2():
+    print("adding code for ML Model2")
+
+
+def model3():
+    print("adding code for ML Model3")
